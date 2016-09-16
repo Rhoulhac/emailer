75af7b8e803447030e14b57a6d9b9b396c515831
GregRhoulhac <greg.rhoulhac@gmail.com>
2016-09-16 10:56:25 -0500

    Trying

--- Python-Emailer-example.py
+++ Python-Emailer-example.py
@@ -1,11 +1,12 @@
 
 emails=[]
 try:
-    email_file = open('email.txt','r')
+    email_file = open('email1.txt','r')
 
     for line in email_file:
             emails.append(line.strip())
 except FileNotFoundError as err:
     print(err)
-    
+
+    #comment is great
 print(emails)
