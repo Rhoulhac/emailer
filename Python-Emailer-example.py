1d79e9446f15e4759add9aa61f2b05fd726b2c24
Greg <greg.rhoulhac@gmail.com>
2016-09-16 20:00:16 -0500

    Added code

--- Python-Emailer-example.py
+++ Python-Emailer-example.py
@@ -1,12 +1,44 @@
+import weather
+import mailer
 
-emails=[]
-try:
-    email_file = open('emails.txt','r')
+    # Reading email from file
+def get_emails():
+    emails = {}
 
-    for line in email_file:
-            emails.append(line.strip())
-except FileNotFoundError as err:
-    print(err)
+    try:
+        email_file = open('emails2.txt','r')
 
-    #comment is great
-print(emails)
+        for line in email_file:
+            (email, name) = line.split(',')
+            emails[email] = name.strip()
+
+    except FileNotFoundError as err:
+        print(err)
+
+    return emails
+
+    # Reading schedule from file
+def get_schedule():
+    try:
+        schedule_file = open('schedule.txt','r')
+
+        schedule = schedule_file.read()
+
+    except FileNotFoundError as err:
+        print(err)
+
+    return schedule
+
+
+
+def main():
+    emails = get_emails()
+
+    schedule = get_schedule()
+
+    forecast = weather.get_weather_forecast()
+
+    mailer.send_emails(emails, schedule, forecast)
+
+main()
+
